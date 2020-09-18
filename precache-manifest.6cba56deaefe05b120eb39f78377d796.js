1786845108f1f66eee30f3d34d9a827c44721c75
Felipe Oliveira <felipe.oliveira@pagtel.com.br>
2020-09-18 15:12:53 -0300

    Subindo projeto

---
+++ precache-manifest.6cba56deaefe05b120eb39f78377d796.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3deb3a1f244765a0fcb3a3362ca1666a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0c8da22e3507b8b099e",
+    "url": "/static/css/2.d7837b9f.chunk.css"
+  },
+  {
+    "revision": "a48fe1aa8be828c98711",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d0c8da22e3507b8b099e",
+    "url": "/static/js/2.2064d12e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2064d12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a48fe1aa8be828c98711",
+    "url": "/static/js/main.dea6c6e7.chunk.js"
+  },
+  {
+    "revision": "214f57ac95931f03df7e",
+    "url": "/static/js/runtime-main.2a35e49e.js"
+  }
+]);
